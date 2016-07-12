c127ddcfd8704045cf2201c1cde027e8350d29f7
Adam Bien <abien@adam-bien.com>
2016-07-12 11:56:09 +0200

    Bulkheads with executor service introduced

--- javaorchestration/src/test/java/com/airhacks/OrchestrationTest.java
+++ javaorchestration/src/test/java/com/airhacks/OrchestrationTest.java
@@ -4,6 +4,9 @@ import com.airhacks.javaorchestration.Archiver;
 import com.airhacks.javaorchestration.NumberProvider;
 import com.airhacks.javaorchestration.Transformer;
 import static java.util.concurrent.CompletableFuture.supplyAsync;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -12,9 +15,16 @@ import org.junit.Test;
  */
 public class OrchestrationTest {
 
+    private ExecutorService executor;
+
+    @Before
+    public void init() {
+        this.executor = Executors.newFixedThreadPool(5);
+    }
+
     @Test
     public void orchestrate() {
-        supplyAsync(NumberProvider::random).
+        supplyAsync(NumberProvider::random, executor).
                 thenApply(Transformer::transform).
                 thenAccept(Archiver::save);
     }
