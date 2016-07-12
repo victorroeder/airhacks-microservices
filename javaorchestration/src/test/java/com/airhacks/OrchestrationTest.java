488c6146e7712d310c55b863dda888148d760126
Adam Bien <abien@adam-bien.com>
2016-07-12 12:10:29 +0200

    Custom rejected execution handler implemented

--- javaorchestration/src/test/java/com/airhacks/OrchestrationTest.java
+++ javaorchestration/src/test/java/com/airhacks/OrchestrationTest.java
@@ -3,9 +3,13 @@ package com.airhacks;
 import com.airhacks.javaorchestration.Archiver;
 import com.airhacks.javaorchestration.NumberProvider;
 import com.airhacks.javaorchestration.Transformer;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import static java.util.concurrent.CompletableFuture.supplyAsync;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -19,14 +23,27 @@ public class OrchestrationTest {
 
     @Before
     public void init() {
-        this.executor = Executors.newFixedThreadPool(5);
+        //   this.executor = Executors.newFixedThreadPool(5);
+        BlockingQueue<Runnable> queue = new ArrayBlockingQueue<>(1);
+        this.executor
+                = new ThreadPoolExecutor(1, 1, 1, TimeUnit.DAYS,
+                        queue,
+                        Executors.defaultThreadFactory(),
+                        this::onOverload);
     }
 
     @Test
     public void orchestrate() {
-        supplyAsync(NumberProvider::random, executor).
-                thenApply(Transformer::transform).
-                thenAccept(Archiver::save);
+        for (int i = 0; i < 10; i++) {
+
+            supplyAsync(NumberProvider::random, executor).
+                    thenApply(Transformer::transform).
+                    thenAccept(Archiver::save);
+        }
+    }
+
+    void onOverload(Runnable r, ThreadPoolExecutor executor) {
+        System.out.println("overloaded = " + executor);
     }
 
 }
